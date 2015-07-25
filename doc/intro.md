d6ec15d80330a8427e44161a38fc5fc5156f24b7
Thomas in't Veld <thomas.intveld@gmail.com>
2015-07-25 13:19:15 +0100

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-classifier
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_classifier/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-classifier.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-classifier.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
